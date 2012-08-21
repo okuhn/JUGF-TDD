797d6c3bfb1e015e9af2cbab97a6899d9b022c6a
oliver <oliver.kuhn@subito.de>
2012-08-21 17:16:39 +0200

    added log output

--- src/test/java/de/jugf/tdd/basic/AssumptionTest.java
+++ src/test/java/de/jugf/tdd/basic/AssumptionTest.java
@@ -2,16 +2,21 @@ package de.jugf.tdd.basic;
 
 import static org.junit.Assert.fail;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hamcrest.core.IsEqual;
 import org.junit.Assume;
 import org.junit.Test;
 
 public class AssumptionTest {
 
+	private static final Log LOG = LogFactory.getLog(ResultExampleTest.class);
+	
 	@Test
 	public void test() {
+		LOG.debug("test with invalid assumption");
 		Assume.assumeThat(1, IsEqual.<Integer>equalTo(2));
-		fail("Not yet implemented");
+		fail("this should not happen");
 	}
 
 }
